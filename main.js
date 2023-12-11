2fd51a160c5d42ed287d8dd315ff65fd636d85fe
FeRRRiJane <103585285+FeRRRiJane@users.noreply.github.com>
2023-12-11 14:21:40 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+       $('.header a').click(function(){
+           /*задали какой мы хотим отступ от верха страницы*/
+           var otstupTop=100;
+           $('body,html').animate({
+           /*получили положение элемента вычли отступ и прокрутили*/
+              scrollTop: $($(this).attr('href')).offset().top-otstupTop
+           }, 1500);
+       });
+   });
+   
